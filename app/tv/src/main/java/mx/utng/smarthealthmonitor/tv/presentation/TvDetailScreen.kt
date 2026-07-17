49bc965e2b9ccf58b035e8928ff8d3343e3a1be7
agustn134 <agustinparralopez1@gmail.com>
2026-07-16 23:33:43 -0600

    feat(tv): add Navigation Compose with catalog/detail/playback routes

---
+++ app/tv/src/main/java/mx/utng/smarthealthmonitor/tv/presentation/TvDetailScreen.kt
@@ -0,0 +1,6 @@
+package mx.utng.smarthealthmonitor.tv.presentation
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.tv.material3.Text
+@Composable
+fun TvDetailScreen(lecturaId: Int, navController: NavController) { Text("Detail $lecturaId") }
